33fea7c5f921334916712c8c9d2f8f287fc43fca
achujel1 <achujel1@gmail.com>
2015-08-03 00:34:30 +0300

    First commit

---
+++ src/org/roba/javabrains/hibernate/HibernateTest.java
@@ -0,0 +1,38 @@
+package org.roba.javabrains.hibernate;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import org.roba.javabrains.dto.UserDetails;
+
+public class HibernateTest {
+	public static void main(String[] args) {
+		UserDetails user = new UserDetails();
+		user.setId(1);
+		user.setUserName("First Name");
+
+		// This is for hibernate 3
+
+		// SessionFactory sessionFactory = new Configuration().configure()
+		// .buildSessionFactory();
+		// Session session = sessionFactory.openSession();
+		// session.beginTransaction();
+		// session.save(user);
+		// session.getTransaction().commit();
+
+		// This is an update for hibernate 4
+
+		Configuration config = new Configuration().configure();
+		ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+				.applySettings(config.getProperties()).build();
+		SessionFactory sessionFactory = config
+				.buildSessionFactory(serviceRegistry);
+		Session session = sessionFactory.openSession();
+		session.beginTransaction();
+		session.save(user);
+		session.getTransaction().commit();
+
+	}
+}
