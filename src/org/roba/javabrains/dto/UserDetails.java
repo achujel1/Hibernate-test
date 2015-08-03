4abed7ba430c8feba9954418f56bb510d231bf37
achujel1 <achujel1@gmail.com>
2015-08-03 20:02:55 +0300

    Part 4 hbm2ddl Configuration and Name Annotations

--- src/org/roba/javabrains/dto/UserDetails.java
+++ src/org/roba/javabrains/dto/UserDetails.java
@@ -1,14 +1,23 @@
 package org.roba.javabrains.dto;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
-@Entity
+/**
+ * Test class to work with hibernate (databases)
+ * 
+ * @author roba
+ *
+ */
+@Entity(name = "USER_DETAILS")
 public class UserDetails {
 
 	@Id
+	@Column(name = "USER_ID")
 	private int id;
 
+	@Column(name = "USER_NAME")
 	private String userName;
 
 	public int getId() {
--- src/org/roba/javabrains/hibernate/HibernateTest.java
+++ src/org/roba/javabrains/hibernate/HibernateTest.java
@@ -7,11 +7,79 @@ import org.hibernate.cfg.Configuration;
 import org.hibernate.service.ServiceRegistry;
 import org.roba.javabrains.dto.UserDetails;
 
+/**
+ * Test class to work with Hibernate
+ * 
+ * @author roba
+ *
+ */
 public class HibernateTest {
+
+	/**
+	 * Main method to work with
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
+
+		// Some code
+
+	}
+
+	/**
+	 * Simple method which tested how column naming work with Hibernate
+	 */
+	private static void testedColumnNamingInHibernate() {
 		UserDetails user = new UserDetails();
 		user.setId(1);
 		user.setUserName("First Name");
+
+		// Using Hibernate 4
+		Configuration config = new Configuration().configure();
+		ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+				.applySettings(config.getProperties()).build();
+		SessionFactory sessionFactory = config
+				.buildSessionFactory(serviceRegistry);
+		Session session = sessionFactory.openSession();
+		session.beginTransaction();
+		session.save(user);
+		session.getTransaction().commit();
+	}
+
+	/**
+	 * This is a method which is testing update and create properties in
+	 * hibernate config file
+	 */
+	private static void testingUpdateAndCreateProperties() {
+		UserDetails user = new UserDetails();
+		user.setId(2);
+		user.setUserName("Second Name");
+
+		// Using Hibernate 4
+		Configuration config = new Configuration().configure();
+		ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+				.applySettings(config.getProperties()).build();
+		SessionFactory sessionFactory = config
+				.buildSessionFactory(serviceRegistry);
+		Session session = sessionFactory.openSession();
+		session.beginTransaction();
+		session.save(user);
+		session.getTransaction().commit();
+	}
+
+	/**
+	 * This is a method which is keeping all used methods
+	 */
+	private static void testedMethods() {
+		firstHibernateTest();
+		testingUpdateAndCreateProperties();
+		testedColumnNamingInHibernate();
+	}
+
+	/**
+	 * Just a simple hibernate test to see if everything is configured well
+	 */
+	private static void firstHibernateTest() {
 
 		// This is for hibernate 3
 
@@ -24,15 +92,6 @@ public class HibernateTest {
 
 		// This is an update for hibernate 4
 
-		Configuration config = new Configuration().configure();
-		ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
-				.applySettings(config.getProperties()).build();
-		SessionFactory sessionFactory = config
-				.buildSessionFactory(serviceRegistry);
-		Session session = sessionFactory.openSession();
-		session.beginTransaction();
-		session.save(user);
-		session.getTransaction().commit();
-
+		testedColumnNamingInHibernate();
 	}
 }
